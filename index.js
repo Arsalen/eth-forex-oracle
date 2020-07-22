300c90e90a42e3a50527e269553409939e9aa746
ahagui <arsalen.hagui@hotmail.com>
2020-07-22 04:15:06 +0100

    immediate probe (tmp)

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var config = require("./config/app.config");
 var { Cron } = require("./src/commons");
 var probe = require("./src/probe");
 
+probe()
+
 var job = new Cron(config.pattern, () => {
     probe();
 })
